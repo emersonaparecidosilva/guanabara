17762a7ef45aefbb1f44fdc99c988250b1301ee9
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-18 11:59:49 -0300

    077_resolvido

--- Python3/exe077.py
+++ Python3/exe077.py
@@ -1,5 +1,13 @@
-#exercicio 077 - Tema XYZ
+#exercicio 077 - Contando vogais em Tupla -> https://www.youtube.com/watch?v=8BgSqrOpKvU&list=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&index=7
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Contando vogais em Tupla')
 print(('==*==')*20)
+
+listagem = ("FACULDADE","QUALIDADE","ENSINO","APRENDIZADO","DEDICACAO","ESFORCO","CRESCIMENTO")
+
+for pos in listagem:
+    print(f'\nNa palavra {pos.upper()} temos: ',end = '')
+    for letra in pos:
+        if letra in 'AEIOUaeiou':
+            print(f'{letra.lower()}',end = ' ')
