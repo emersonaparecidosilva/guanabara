844ac5ae1c1bfae2d31ef1f876d51b9530ec8b66
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-18 16:55:23 -0300

    079_resolvido

--- Python3/exe079.py
+++ Python3/exe079.py
@@ -1,5 +1,28 @@
-#exercicio 079 - Tema XYZ
+#exercicio 079 - Exercício Python #079 - Valores únicos em uma Lista
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Valores únicos em uma Lista')
 print(('==*==')*20)
+
+listagem = []
+contador = 0
+soma = 0
+
+while True:
+    número = int(input('Digite um número: '))
+    while número in listagem:
+         print('Esse número já tem na lista')
+         número = int(input('Digite um número: '))
+    listagem.append(número)
+    print('Valor adicionado com sucesso!')
+    contador +=1
+    soma += número
+    continuar = input('Deseja continuar? [S/N]: ').strip().upper()
+    while continuar not in 'SN':
+         continuar = input('Deseja continuar? [S/N]: ').strip().upper()
+    if continuar == 'N':
+         break
+listagemOrdernada = listagem.sort()
+print()
+print(f'Você digitou {contador} valores únicos, são eles: {listagem}!')
+print(f'A soma dos itens digitados é: {soma}!')
