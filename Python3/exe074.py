739b93fef68b2a67531878ccbfacef3f3024ca2d
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-18 10:37:46 -0300

    074_resolvido

--- Python3/exe074.py
+++ Python3/exe074.py
@@ -1,5 +1,11 @@
-#exercicio 074 - Tema XYZ
+#exercicio 074 - Exercício Python #074 - Maior e menor valores em Tupla -> https://www.youtube.com/watch?v=mlwt2CRQkTQ&list=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&index=4
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Exercício Python #074 - Maior e menor valores em Tupla')
 print(('==*==')*20)
+
+from random import randint
+
+tupla = [randint(1, 40) for a in range(5)]
+print(f'Os itens sorteados foram: {(tupla)} Sendo: o menor: {min(tupla)} e o maior: {max(tupla)}')
+print(f'Itens sorteados em ordem: {sorted(tupla)}')
