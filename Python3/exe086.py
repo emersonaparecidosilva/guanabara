f9c871ba9cd4396ac1261d1e32c0f1825b73129b
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-20 15:58:08 -0300

    086_resolvido

--- Python3/exe086.py
+++ Python3/exe086.py
@@ -1,5 +1,13 @@
-#exercicio 086 - Tema XYZ
+#Exercício Python #086 - Matriz em Python - https://www.youtube.com/watch?v=EGmlFdwD4C4&list=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&index=18
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Matriz em Python')
 print(('==*==')*20)
+
+numeros = [[],[],[]],[[],[],[]],[[],[],[]]
+for e in range(0,3):
+    for i in range(0,3):
+        número = int(input(f'Digite um valor para [{e},{i} ]: '))
+        numeros[e][i].append(número)
+for p,i in enumerate(numeros):
+    print(f'{numeros[p]}')
