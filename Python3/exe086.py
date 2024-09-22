276c6110e68d0bb853bc6b85bb0e6e977166db5c
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-22 13:05:33 -0300

    086_refatorado

--- Python3/exe086.py
+++ Python3/exe086.py
@@ -4,10 +4,13 @@ print(('==*==')*20)
 print('Bem vindo(a) - Matriz em Python')
 print(('==*==')*20)
 
-numeros = [[],[],[]],[[],[],[]],[[],[],[]]
+numeros = []
 for e in range(0,3):
+    temp = []
     for i in range(0,3):
         número = int(input(f'Digite um valor para [{e},{i} ]: '))
-        numeros[e][i].append(número)
+        temp.append(número)
+    numeros.append(temp)
 for p,i in enumerate(numeros):
     print(f'{numeros[p]}')
+    
