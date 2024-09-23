43b2d0470075db5c29412d4e03b9d2addfb70521
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-23 11:44:28 -0300

    090_resolvido

--- Python3/exe090.py
+++ Python3/exe090.py
@@ -1,5 +1,18 @@
-#exercicio 090 - Tema XYZ
+#Exercício Python #090 - Dicionário em Python -> https://www.youtube.com/watch?v=HipQYUk4koA&list=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&index=23
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Dicionário em Python')
 print(('==*==')*20)
+
+aluno = {}
+aluno['nome'] = input("Nome: ")
+aluno['média']= float(input("Média: "))
+if aluno['média'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif aluno['média'] > 5 and aluno['média'] < 7:
+    aluno['situacao'] = 'Aprovado'
+else:
+    aluno['situacao'] = 'Reprovado'
+print(('==*==')*20)
+for k, v in aluno.items():
+    print(f'- {k} é igual a {v}')
