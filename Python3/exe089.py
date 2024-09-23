9241eed9439e7979fe30da9bc6a579557c30d44c
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-22 22:51:41 -0300

    089_iniciado

--- Python3/exe089.py
+++ Python3/exe089.py
@@ -1,5 +1,25 @@
-#exercicio 089 - Tema XYZ
+#Exercício Python #089 - Boletim com listas compostas -> https://www.youtube.com/watch?v=7xrCJnniqMw&list=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&index=21
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Boletim com listas compostas')
 print(('==*==')*20)
+
+alunos = []
+while True:
+    alunosTemp = []
+    while True:
+        nome = input('Nome: ')
+        nota1 = float(input('Nota 1: '))
+        nota2 = float(input('Nota 2: '))
+        alunoMedia = (nota1+nota2)/2
+        alunosTemp.append([nome,nota1,nota2,alunoMedia])
+        continuar = input('Deseja continuar? [S/N]: ').strip().upper()
+        while continuar not in 'SN':
+            continuar = input('Deseja continuar? [S/N]: ').strip().upper()
+        if continuar == 'N':
+            break
+    alunos.append(alunosTemp)
+    break
+for p,i in enumerate(alunos):
+    print(f'No {[p+1]}: Nome: {alunos[0][p][0]} Média: {alunos[0][p][3]:.2f}')
+        
