97c6ad3ec04af82c3ec2bae0261128bf7e5353d1
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-26 12:47:54 -0300

    097_resolvido

--- Python3/exe097.py
+++ Python3/exe097.py
@@ -1,5 +1,16 @@
-#exercicio 097 - Tema XYZ
+#Exercício Python #097 - Um print especial
 
-print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
-print(('==*==')*20)
+def escreva(txt):
+    a = (len(txt))+4
+    print(('=')*a)
+    print(f'{txt:^{a}}')
+    print(('=')*a)
+
+escreva('Bem vindo(a) - Um print especial')
+
+escreva("Emerson Silva")
+escreva("Curso de Python no Youtube")
+escreva("CeV")
+
+
+
