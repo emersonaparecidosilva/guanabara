f6af2b08c6e136b21b33b9d32fe0cbe567a17bcd
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-26 13:43:39 -0300

    099_resolvido

--- Python3/exe099.py
+++ Python3/exe099.py
@@ -1,5 +1,30 @@
-#exercicio 099 - Tema XYZ
+#Exercício Python #099 - Função que descobre o maior
 
-print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
-print(('==*==')*20)
+def escreva(txt):
+    a = (len(txt))+4
+    print(('=')*a)
+    print(f'{txt:^{a}}')
+    print(('=')*a)
+
+escreva('Bem vindo(a) - Função que descobre o maior')
+print()
+from time import sleep
+
+def maior(* num):
+    print(('===')*30)
+    print('Processando..')
+    contador = maior = 0
+    for e in num:
+        print(f'{e}', end = ' ',flush=True)
+        if e >= maior:
+            maior = e
+        contador +=1
+        sleep(0.5)
+    print(f'Foram informados {contador} valores ao todo') 
+    print(f'O maior valor informado foi {maior}') 
+
+maior(2,9,4,51,7,1)
+maior(4,70,0)
+maior(1,2)
+maior(6)
+maior()
