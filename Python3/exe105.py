862597578b8e41099ef2f762c8b4d20f785c1e72
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-30 13:38:27 -0300

    105_resolvido

--- Python3/exe105.py
+++ Python3/exe105.py
@@ -1,5 +1,36 @@
 #exercicio 105 - Tema XYZ
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Exercício Python #105 - Analisando e gerando Dicionários')
 print(('==*==')*20)
+
+def notas(*notas,sit=False):
+    """_Função criada para analisar notas e situações de diversos alunos_
+
+    Args:
+        sit (bool, optional): Exibir ou não a situação do aluno
+
+    Returns:
+        dict: Total de notas, Maior, Menor e Média.
+    """
+    soma = 0
+    situacao = ''
+    resultado = {}
+    for c in notas:
+        soma += c
+    total = len(notas)
+    média = soma/total
+    if média < 6:
+        situacao = 'RUIM'
+    elif média >=6 and média <8:
+        situacao = 'REGULAR'
+    else:
+        situacao = 'BOA'
+    resultado.update({"Total": total, "Maior": max(notas),"Menor": min(notas), "Média": f'{média:.2f}' } )
+    if sit:
+        resultado.update({"Situação": situacao})
+        print(resultado)
+    else:
+        print(resultado)
+notas(10,10,8,5,4,2,9,9,sit=True)
+help(notas)
