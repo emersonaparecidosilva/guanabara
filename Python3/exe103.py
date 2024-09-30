f5bda87633f004c24e107f8f986c330f9311bbf3
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-30 12:44:42 -0300

    103_resolvido

--- Python3/exe103.py
+++ Python3/exe103.py
@@ -1,5 +1,19 @@
-#exercicio 103 - Tema XYZ
+#Exercício Python #103 - Ficha do Jogador
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Ficha do Jogador')
 print(('==*==')*20)
+
+def ficha(jog = '<Desconhecido>',gol = 0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato!')
+
+nome = str(input('Nome do Jogador: ')).strip()
+gols = str(input('Número de Gols: '))
+if gols.isnumeric():
+    gols=int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(gol = gols)
+else:
+    ficha(nome,gols)
