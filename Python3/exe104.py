23f12b8722bf94515980222c11a0afe0864605cc
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-09-30 13:07:10 -0300

    104_resolvido

--- Python3/exe104.py
+++ Python3/exe104.py
@@ -1,5 +1,22 @@
-#exercicio 104 - Tema XYZ
+#Exercício Python #104 - Validando entrada de dados em Python
 
 print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
+print('Bem vindo(a) - Validando entrada de dados em Python')
 print(('==*==')*20)
+
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = input(msg)
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print(f'\33[0;31mERRO! Digite um número inteiro válido.\33[m')
+        if ok:
+            break
+    return valor
+
+n = leiaint('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
