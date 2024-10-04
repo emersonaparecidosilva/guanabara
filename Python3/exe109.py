f0c3f63908a1a59d2396f68ec81f318d19fc4d91
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-10-04 10:03:27 -0300

    110_resolvido

--- Python3/exe109.py
+++ Python3/exe109.py
@@ -4,13 +4,9 @@
 import sys,os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'modulos'))) # Adiciona a pasta 'modulos' ao sys.path
 
-from moedas import aumentar,diminuir,dobro,metade,moeda
+from moedas import aumentar,diminuir,dobro,metade,moeda,resumo
 from helpers import título
 
 título(' Formatando Moedas em Python - Continuação',2)
-num = int(input('Digite o preço: R$ '))
-
-print(f'A metade de {moeda(num)} é {metade(num,True)}.')
-print(f'O dobro de {moeda(num)} é {dobro(num,True)}.')
-print(f'Aumentando 10% de {moeda(num)} é {aumentar(num,10,True)}.')
-print(f'Diminuindo 10% de {moeda(num)} é {diminuir(num,10,True)}.')
+num = float(input('Digite o preço: R$ '))
+resumo(num,80,35)
--- modulos/moedas.py
+++ modulos/moedas.py
@@ -2,7 +2,6 @@
 Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
  Faça também um programa que importe esse módulo e use algumas dessas funções.
 '''
-
 def aumentar(valor,fator=0, format=False):
     calculo = valor * (1+(fator/100))
     novoValor = round(calculo,2)
@@ -26,4 +25,17 @@ def metade(valor, format=False):
 def moeda(valor=0,pais='R$'):
     return f'{pais}{valor:.2f}'.replace('.',',')
 
+def resumo(valor,aumenta,diminui):
+    print('-'*40)
+    print('Resumo do Valor'.center(40))
+    print('-'*40)
+    print(f'Preço analisado: \t{moeda(valor)}')
+    print(f'Dobro do Preço: \t{dobro(valor,True)}')
+    print(f'Metade do Preço: \t{metade(valor,True)}')
+    print(f'{aumenta}% de Aumento: \t{aumentar(valor,aumenta,True)}')
+    print(f'{diminui}% de Redução: \t{diminuir(valor,diminui,True)}')
+    print('-'*40)
+    
+
 # print(moeda(20,'USS ')) #Exemplo Implementação Moeda
+# resumo(200,10,20) #Exemplo Implementação Resumo
