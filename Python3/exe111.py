7717008c80a799db228d61eef58e6fa450cfdaf8
emersonaparecidosilva <emersonaparecidosilva@gmail.com>
2024-10-04 10:24:29 -0300

    111_resolvido

--- Python3/exe111.py
+++ Python3/exe111.py
@@ -1,5 +1,11 @@
-#exercicio 111 - Tema XYZ
+#Exercício Python #111 - Transformando módulos em pacotes
 
-print(('==*==')*20)
-print('Bem vindo(a) - Tema XYZ')
-print(('==*==')*20)
+import sys,os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'modulos'))) # Adiciona a pasta 'modulos' ao sys.path
+
+from helpers import título
+from moedas import resumo
+
+título(' Formatando Moedas em Python - Resumo',2)
+num = float(input('Digite o preço: R$ '))
+resumo(num,80,35)
